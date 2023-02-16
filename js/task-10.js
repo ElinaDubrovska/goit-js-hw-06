e6236aecc15e824ba3b8841f3e53f8434827504a
ElinaDubrovska <elinadubrovska1801@gmail.com>
2023-02-16 09:41:51 +0200

    add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
+
+
+const createBtn = document.querySelector("button[data-create]");
+createBtn.addEventListener("click", () => {
+  createBoxes(inputEl.value);
+});
+
+const destroyBtn = document.querySelector("button[data-destroy]");
+destroyBtn.addEventListener("click", () => {
+  divBox.innerHTML = "";
+  inputEl.value = "";
+});
+
+const inputEl = document.querySelector("div>input");
+
+const divBox = document.querySelector("#boxes");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const newDiv = document.createElement("div");
+    newDiv.style.width = `${30 + 10 * i}px`;
+    newDiv.style.height = `${30 + 10 * i}px`;
+    newDiv.style.backgroundColor = getRandomHexColor();
+    divBox.append(newDiv);
+  }
 }
